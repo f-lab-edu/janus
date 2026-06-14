9aa08f83564ef079ba6e4a8e75032cdf39720501
oniwon <cookie89800@naver.com>
2026-06-15 01:09:43 +0900

    feat: MySQL Docker 및 Profile별 설정 구성

---
+++ src/main/java/kr/ai/janus/JanusBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.ai.janus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanusBackendApplication.class, args);
+    }
+
+}
